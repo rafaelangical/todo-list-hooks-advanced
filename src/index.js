b2681d73d5e76af188f03ad0384101f463ed1a4a
rafaelangical <rafaelangical2@gmail.com>
2019-09-12 16:36:29 -0300

    Remove style.css

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import { Provider } from 'react-redux';
 import Store from './store';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
-import './index.css';
 import { PersistGate } from 'redux-persist/integration/react';
 
 const { persistor, store } = Store();
