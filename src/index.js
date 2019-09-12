55eb4fd7692c4b123be42a253f4084e7e36ed4a6
rafaelangical <rafaelangical2@gmail.com>
2019-09-12 16:34:16 -0300

    Import store and update

--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import Store from './store';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
+import './index.css';
+import { PersistGate } from 'redux-persist/integration/react';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const { persistor, store } = Store();
 
+ReactDOM.render(
+	<Provider store={store}>
+		<PersistGate loading={null} persistor={persistor}>
+			<App />
+		</PersistGate>
+	</Provider>,
+	document.getElementById('root')
+);
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
